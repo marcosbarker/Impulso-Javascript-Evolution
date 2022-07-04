280ef840fca47619171d678294564ca8268ad76a
Marcos Paulo Marques Correa Gomes <marcos_bareker@hotmail.com>
2022-07-04 16:09:01 -0300

    code: update

---
+++ introducaoAoJs/contador/assets/js/script.js
@@ -0,0 +1,18 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+}
+
+function decrement() {
+
+    if(currentNumber > 0) {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    }else{                
+        return alert("Contador em 0");
+    }
+}
